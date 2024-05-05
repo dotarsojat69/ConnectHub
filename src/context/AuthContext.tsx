6185541d59061c9c7bf673f2e270cc37233ac89b
dotarsojat69 <tdeath457@gmail.com>
2024-05-06 00:36:58 +0700

    feat: add query auth and query provider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -32,64 +32,62 @@ export const INITIAL_USER = {
 
 const AuthContext = createContext<IContextType>(INITIAL_STATE);
 
-const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-    const navigate = useNavigate();
-    const [user, setUser] = useState<IUser>(INITIAL_USER);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const checkAuthUser = async () => {
-        setIsLoading(true);
-        try {
-          const currentAccount = await getCurrentUser();
-          if (currentAccount) {
-            setUser({
-              id: currentAccount.$id,
-              name: currentAccount.name,
-              username: currentAccount.username,
-              email: currentAccount.email,
-              imageUrl: currentAccount.imageUrl,
-              bio: currentAccount.bio,
-            });
-            setIsAuthenticated(true);
-    
-            return true;
-          }
+  const checkAuthUser = async () => {
+    setIsLoading(true);
+    try {
+      const currentAccount = await getCurrentUser();
+      if (currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio,
+        });
+        setIsAuthenticated(true);
 
-          return false;
-        } catch (error) {
-            console.error(error);
-            return false;
-          } finally {
-            setIsLoading(false);
-          }
-        };
+        return true;
+      }
 
-        useEffect(() => {
-            const cookieFallback = localStorage.getItem("cookieFallback");
-            if (
-              cookieFallback === "[]" ||
-              cookieFallback === null ||
-              cookieFallback === undefined
-            ) {
-              navigate("/sign-in");
-            }
-        
-            checkAuthUser();
-          }, []);
-        
-          const value = {
-            user,
-            setUser,
-            isLoading,
-            isAuthenticated,
-            setIsAuthenticated,
-            checkAuthUser,
-          };
-        
-          return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+      return false;
+    } catch (error) {
+      console.error(error);
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const cookieFallback = localStorage.getItem("cookieFallback");
+    if (
+      cookieFallback === "[]" ||
+      cookieFallback === null ||
+      cookieFallback === undefined
+    ) {
+      navigate("/sign-in");
+    }
+
+    checkAuthUser();
+  }, []);
+
+  const value = {
+    user,
+    setUser,
+    isLoading,
+    isAuthenticated,
+    setIsAuthenticated,
+    checkAuthUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
-
-export default AuthProvider;
 
 export const useUserContext = () => useContext(AuthContext);
--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -10,13 +10,11 @@ import { INewUser } from "@/types"
 export const useCreateAccount = () => {
     return useMutation({
         mutationFn: (user: INewUser) => createUserAccount(user)
-    })
-}
+    });
+};
 
 export const useLoginAccount = () => {
     return useMutation({
-        mutationFn: (user: {email: string;
-            password: string;
-        }) => loginAccount(user),
+        mutationFn: (user: {email: string; password: string }) => loginAccount(user),
     });
-}
+};
