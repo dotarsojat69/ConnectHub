1012ba331690e7f4ea8d8a427e4594703a3f172e
dotarsojat69 <tdeath457@gmail.com>
2024-05-06 00:35:45 +0700

    feat: add login validation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,8 +1,13 @@
 import * as z from "zod"
 
-export const LoginSchema = z.object({
+export const RegisterSchema = z.object({
     name: z.string().min(6, { message: "Too short"}),
     username: z.string().min(8, { message: "Too short"}),
     email: z.string().email(),
     password: z.string().min(6, { message: "Password must be at least 6 characters."}),
+  })
+
+  export const LoginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, { message: "Password must be at least 6 characters."}),
   })
