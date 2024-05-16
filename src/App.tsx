f2bd1b29bf8166cf6953684aa452954ee6e153fa
dotarsojat69 <tdeath457@gmail.com>
2024-05-16 10:21:17 +0700

    feat: add routes for page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "./_auth/forms/Login";
 import Register from "./_auth/forms/Register";
-import Home from "./_root/pages/Home";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from "./_root/pages";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
 
@@ -22,6 +22,14 @@ const App = () => {
             {/* private routes */}
             <Route element={<RootLayout />}>
             <Route index element={<Home />}/>
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/saved" element={<Saved />} />
+            <Route path="/all-users" element={<AllUsers />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post/:id" element={<EditPost />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+            <Route path="/profile/:id/*" element={<Profile />} />
+            <Route path="/update-profile/:id" element={<UpdateProfile />} />
             </Route>
         </Routes>
         <Toaster />
