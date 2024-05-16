160bd55a0bf2b62d2fb6bd9c30810df3b4ab4bc4
dotarsojat69 <tdeath457@gmail.com>
2024-05-16 10:21:52 +0700

    feat: add responsive in layout

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Outlet } from "react-router-dom"
+
+import Topbar from "@/components/custom/Topbar"
+import Bottombar from "@/components/custom/Bottombar"
+import Sidebar from "@/components/custom/Sidebar"
+
 
 const RootLayout = () => {
   return (
-    <div>
-      RootLayput
+    <div className="w-full md:flex">
+      <Topbar />
+      <Sidebar />
+
+      <section className="flex flex-1 h-full"> 
+        <Outlet />
+      </section>
+
+      <Bottombar />
     </div>
   )
 }
