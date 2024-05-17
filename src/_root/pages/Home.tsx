ebc299c5d2b78bfc95b11f570c79545f1ce67dde
dotarsojat69 <tdeath457@gmail.com>
2024-05-17 13:09:23 +0700

    feat: finishing homepage

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,11 +1,27 @@
+import Loader from "@/components/custom/Loader";
+import PostCard from "@/components/custom/PostCard";
+import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutation";
+import { Models } from "appwrite";
 
 
 const Home = () => {
+  const { data: posts, isPending: isPostLoading, isError:
+    isErrorPosts } = useGetRecentPosts();
+
   return (
     <div className="flex flex-1">
-      <div className="common-container">
-        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
-          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Timeline</h2>
+          {isPostLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {posts?.documents.map((post: Models.Document) => (
+                <PostCard post={post} />
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
