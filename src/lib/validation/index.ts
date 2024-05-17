391a168cc57a7c3d599aa567a6cee9520511b298
dotarsojat69 <tdeath457@gmail.com>
2024-05-17 16:28:48 +0700

    feat: add profile validation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,20 +1,28 @@
-import * as z from "zod"
+import * as z from "zod";
 
 export const RegisterSchema = z.object({
     name: z.string().min(6, { message: "Too short"}),
     username: z.string().min(8, { message: "Too short"}),
     email: z.string().email(),
     password: z.string().min(6, { message: "Password must be at least 6 characters."}),
-  })
+  });
 
   export const LoginSchema = z.object({
     email: z.string().email(),
     password: z.string().min(8, { message: "Password must be at least 6 characters."}),
-  })
+  });
 
   export const PostsSchema = z.object({
     caption: z.string().min(5).max(2200),
     file: z.custom<File[]>(),
     location: z.string().min(6).max(100),
     tags: z.string(),
-  })
+  });
+
+  export const ProfileValidation = z.object({
+    file: z.custom<File[]>(),
+    name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    username: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    email: z.string().email(),
+    bio: z.string(),
+  });
