9212aac83003589fda23c66384e9ad2298d652a4
dotarsojat69 <tdeath457@gmail.com>
2024-05-17 16:25:46 +0700

    feat: add strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
 import AuthProvider from "./context/AuthContext";
 import { QueryProvider } from "./lib/react-query/QueryProvider";
 
+import App from "./App";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+<React.StrictMode>    
 <BrowserRouter>
 <QueryProvider>
 <AuthProvider>  
@@ -14,4 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 </AuthProvider>
 </QueryProvider>
 </ BrowserRouter>
+</React.StrictMode>
 )
