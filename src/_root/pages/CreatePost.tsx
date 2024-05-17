c36bdf49966859a7e766ed5641ce59c52a1b051f
dotarsojat69 <tdeath457@gmail.com>
2024-05-17 13:06:08 +0700

    feat: add component to page

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,6 +1,7 @@
-
+import PostForm from "@/components/forms/PostForm"
 
 const CreatePost = () => {
+
   return (
     <div className="flex flex-1">
       <div className="common-container">
@@ -14,6 +15,8 @@ const CreatePost = () => {
           />
           <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
         </div>
+
+        <PostForm />
       </div>
     </div>
   )
