2bed85bb892871b174391793d094d62501947914
dotarsojat69 <tdeath457@gmail.com>
2024-05-17 13:07:20 +0700

    feat: add validation for create post

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -11,3 +11,10 @@ export const RegisterSchema = z.object({
     email: z.string().email(),
     password: z.string().min(8, { message: "Password must be at least 6 characters."}),
   })
+
+  export const PostsSchema = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(6).max(100),
+    tags: z.string(),
+  })
