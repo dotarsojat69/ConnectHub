f76b93c4cd3e88ca3f8b4d372442e99c1ce0e7fe
dotarsojat69 <tdeath457@gmail.com>
2024-05-19 22:00:07 +0700

    add: add some change

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -23,8 +23,8 @@ interface StabBlockProps {
 
 const StatBlock = ({ value, label }: StabBlockProps) => (
   <div className="flex-center gap-2">
-    <p className="small-semibold lg:body-bold text-primary-500">{value}</p>
-    <p className="small-medium lg:base-medium text-light-2">{label}</p>
+    <p className="small-semibold lg:body-bold text-light-4">{value}</p>
+    <p className="small-medium lg:base-medium text-light-3">{label}</p>
   </div>
 );
 
@@ -65,8 +65,8 @@ const Profile = () => {
 
             <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
               <StatBlock value={currentUser.posts.length} label="Posts" />
-              <StatBlock value={20} label="Followers" />
-              <StatBlock value={20} label="Following" />
+              <StatBlock value={0} label="Followers" />
+              <StatBlock value={0} label="Following" />
             </div>
 
             <p className="small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm">
@@ -78,7 +78,7 @@ const Profile = () => {
             <div className={`${user.id !== currentUser.$id && "hidden"}`}>
               <Link
                 to={`/update-profile/${currentUser.$id}`}
-                className={`h-12 bg-dark-4 px-5 text-light-1 flex-center gap-2 rounded-lg ${
+                className={`h-12 bg-dark-4 px-5 text-light-4 flex-center gap-2 rounded-lg ${
                   user.id !== currentUser.$id && "hidden"
                 }`}>
                 <img
