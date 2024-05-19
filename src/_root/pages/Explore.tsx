205b6ffdb54bddbeb3fdbdb51c89104805e1cf88
dotarsojat69 <tdeath457@gmail.com>
2024-05-19 22:01:17 +0700

    feat: change some color

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -77,7 +77,7 @@ const Explore = () => {
         <h3 className="body-bold md:h3-bold">Popular Today</h3>
 
         <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
-          <p className="small-medium md:base-medium text-light-2">All</p>
+          <p className="small-medium md:base-medium text-black">All</p>
           <img
             src="/assets/icons/filter.svg"
             width={20}
