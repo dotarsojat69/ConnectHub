37f3c768f75aa9b7a6947473651cf044daf81e0d
dotarsojat69 <tdeath457@gmail.com>
2024-05-19 20:17:12 +0700

    feat: add save like page

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import Loader from "@/components/custom/Loader";
+import GridPostList from "@/components/custom/GridPostList";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutation";
 
 const LikedPosts = () => {
-  return (
-    <div>
-      Liked
-    </div>
-  )
-}
+  const { data: currentUser } = useGetCurrentUser();
 
-export default LikedPosts
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
+  return (
+    <>
+      {currentUser.liked.length === 0 && (
+        <p className="text-light-4">No liked posts</p>
+      )}
+
+      <GridPostList posts={currentUser.liked} showStats={false} />
+    </>
+  );
+};
+
+export default LikedPosts;
