bed81be6e5d68bf61b4f2bc114fcb337ee3c0683
dotarsojat69 <tdeath457@gmail.com>
2024-06-26 16:20:12 +0700

    feat: resize

--- src/_auth/forms/Login.tsx
+++ src/_auth/forms/Login.tsx
@@ -59,7 +59,9 @@ const handleLogin = async (user: z.infer<typeof LoginSchema>) => {
   return (
       <Form {...form}>
            <div className="sm:w-420 flex-center flex-col">
-            <img src="/assets/images/logo-new.png" alt="logo" />
+            <img src="/assets/images/logo-new.png" alt="logo" 
+            className="w-[50px] h-[50px]"
+            />
 
             <h2 className="h3-bold md:h2-bold pt-5 sm:pt-8">
                 Login to your account
