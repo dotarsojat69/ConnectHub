b96f93429b9bf26ec83096bdc95cefd6ff5066eb
dotarsojat69 <tdeath457@gmail.com>
2024-06-26 16:05:30 +0700

    feat: update logo

--- src/_auth/forms/Register.tsx
+++ src/_auth/forms/Register.tsx
@@ -76,7 +76,7 @@ const handleRegister = async (user: z.infer<typeof RegisterSchema>) => {
   return (
       <Form {...form}>
            <div className="sm:w-420 flex-center flex-col">
-            <img src="/assets/images/logo.svg" alt="logo" />
+            <img src="/assets/images/logo-new.svg" alt="logo" />
 
             <h2 className="h3-bold md:h2-bold pt-5 sm:pt-8">
                 Create a new account
